19f0e6cfa7741aaa677eb85e90fecb38732b82e2
ISadSmile <sadsmilegames@gmail.com>
2018-08-12 01:11:06 +0200

    Added DirectionChanged Event to PlayerEffectController. Made it possible to render the weapon correctly.

--- Jonah Remastered/Assets/Scripts/Weapons/WeaponEffectsManager.cs
+++ Jonah Remastered/Assets/Scripts/Weapons/WeaponEffectsManager.cs
@@ -4,13 +4,52 @@ using UnityEngine;
 
 public class WeaponEffectsManager : MonoBehaviour
 {
-	// Use this for initialization
-	void Start () {
-		
+    public GameObject graphics;
+    private SpriteRenderer spriteRenderer;
+
+    private void OnEnable()
+    {
+        PlayerEffectsController.OnDirectionChangedEvent += ChangeLookDirection;
+    }
+
+    private void OnDisable()
+    {
+        PlayerEffectsController.OnDirectionChangedEvent -= ChangeLookDirection;
+    }
+
+    void Start ()
+    {
+        spriteRenderer = graphics.GetComponent<SpriteRenderer>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void ChangeLookDirection(int direction)
+    {
+        switch(direction)
+        {
+            case 1:
+                ResetSpriteRenderer();
+                spriteRenderer.sortingOrder = -1;
+                break;
+            case 2:
+                ResetSpriteRenderer();
+                break;
+            case 3:
+                ResetSpriteRenderer();
+                break;
+            case 4:
+                ResetSpriteRenderer();
+                spriteRenderer.flipY = true;
+                break;
+            default:
+                Debug.LogWarning("The event fired made no sense");
+                break;
+        }
+    }
+
+    private void ResetSpriteRenderer()
+    {
+        spriteRenderer.flipX = false;
+        spriteRenderer.flipY = false;
+        spriteRenderer.sortingOrder = 0;
+    }
 }
