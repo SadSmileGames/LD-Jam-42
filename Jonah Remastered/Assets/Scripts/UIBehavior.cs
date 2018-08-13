23400375999900b5bb072ae3c15e53e9c1dbb41d
Julllan <julian@vu-online.de>
2018-08-13 18:37:25 +0200

    UI created

---
+++ Jonah Remastered/Assets/Scripts/UIBehavior.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBehavior : MonoBehaviour {
+
+    public GameObject[] textBoxes;
+    public GameObject[] health;
+    public GameObject blackBox;
+
+    private Text killCountText;
+    private Text scoreText;
+    private int killCount;
+    private BlackFader fader;
+    // Use this for initialization
+    void Start()
+    { 
+        killCountText = textBoxes[0].GetComponent<Text>();
+        fader = blackBox.GetComponent<BlackFader>();
+        scoreText = textBoxes[3].GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+
+    private void disableText()
+    {
+        foreach(GameObject box in textBoxes)
+        {
+            box.SetActive(false);
+        }
+    }
+
+    public void onKill(int kills)
+    {
+        killCount = kills;
+        killCountText.text = ("Kills: " + killCount);
+    }
+
+    public void damaged()
+    {
+        if (health[4].activeSelf)
+        {
+            health[4].SetActive(false);
+        }
+        else if (health[3].activeSelf)
+        {
+            health[3].SetActive(false);
+        }
+        else if (health[2].activeSelf)
+        {
+            health[2].SetActive(false);
+        }
+        else if (health[1].activeSelf)
+        {
+            health[1].SetActive(false);
+        }
+        else if (health[0].activeSelf)
+        {
+            health[0].SetActive(false);
+        }
+    }
+
+    public void gameOver()
+    {
+        disableText();
+        fader.fadeIn();
+        scoreText.text = ("Score: " + killCount);
+        textBoxes[3].SetActive(true);
+        textBoxes[2].SetActive(true);
+    }
+
+}
